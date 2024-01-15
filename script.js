cd42e73148b553edecaf49534a762a9e22bc1f31
Kravtsev Artem <arkravt@gmail.com>
2024-01-15 12:46:48 +0400

    add sticky navigation

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const tabContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
 const operationsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 ///////////////////////////////////////
 // Modal window
@@ -93,11 +94,9 @@ tabContainer.addEventListener('click', (e) => {
 
 ///////////////////////////////////////
 // Анимация потускнения на панели навигации
-
 // TARGET - тот элемент на который нажали
 // CURRENTTARGET - тот элемент к которому прикреплен обработчик
 // THIS - тот элемент к которому прикреплен обработчик (this = currentTarget)
-
 const navLinksHoverAnimation = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const linkOver = e.target;
@@ -114,8 +113,44 @@ const navLinksHoverAnimation = function (e) {
     text.style.opacity = this;
   }
 };
-
 nav.addEventListener('mouseover', navLinksHoverAnimation.bind(0.4));
 nav.addEventListener('mouseout', navLinksHoverAnimation.bind(1));
 
 
+///////////////////////////////////////
+// Sticky navigation  
+// Bad solution, because scroll events happen every scroll
+// const section1Coords = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > section1Coords.top) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   };
+// });
+
+// Good solution - Intersection Observer API
+// Intersection Observer API (IOA) позволяет приложению асинхронно наблюдать за пересечением элемента (target) с его родителем (root) или областью просмотра (viewport). 
+//Другими словами, этот API обеспечивает вызов определенной функции каждый раз при пересечении целевого элемента с root или viewport.
+const navHeight = nav.getBoundingClientRect().height;
+const observerCallBack = function (entries, observer) {
+
+  const entry = entries[0];
+  
+  //nav.classList.toggle('sticky'); ИЛИ
+  if (entry.isIntersecting) {
+    nav.classList.remove('sticky');
+  } else {
+    nav.classList.add('sticky');
+  };
+
+};
+
+const observerOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+};
+
+const observer = new IntersectionObserver(observerCallBack, observerOptions);
+observer.observe(header);
